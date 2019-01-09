f5e4850d6023755b74a1454f866300bb92e7acf5
itssomething <vdmanh107@gmail.com>
2019-01-09 11:10:30 +0700

    format message

--- app/controllers/cws_controller.rb
+++ app/controllers/cws_controller.rb
@@ -19,8 +19,7 @@ class CwsController < ApplicationController
 
     # set up some params
     destination_room_id = ENV["DESTINATION_ROOM_ID"]
-    body = "[To:3056978] Vu Duc Manh (private)\n
-      Message in #{room_name}:\n#{body}"
+    body = "[To:3056978] Vu Duc Manh (private)\nMessage in #{room_name}:\n#{body}"
 
     # change token to bot's token
     ChatWork.api_key = ENV["BOT_TOKEN"]
