708b24378fcbf0de15ac2c8461824bf7726cd58f
itssomething <vdmanh107@gmail.com>
2019-01-09 09:53:55 +0700

    skip csrf

--- app/controllers/cws_controller.rb
+++ app/controllers/cws_controller.rb
@@ -1,9 +1,12 @@
 class CwsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render html: "index page"
   end
 
   def create
     puts params.to_s
+    render status: 200, json: params
   end
 end
