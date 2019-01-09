e66d2811bdb1cd1b1ad5e54180f38e21b12f23bd
itssomething <vdmanh107@gmail.com>
2019-01-09 10:31:15 +0700

    added some comments

--- app/controllers/cws_controller.rb
+++ app/controllers/cws_controller.rb
@@ -6,13 +6,19 @@ class CwsController < ApplicationController
   end
 
   def create
-    puts params.to_s
+    # get room id and body of the message
     room_id = params[:webhook_event][:room_id]
     body = params[:webhook_event][:body]
+
+    # find room
     c = ChatWork::Room.find room_id: room_id
     room_name = c.name
+
+    # set up some params
     destination_room_id = 137369783
     body = "Message in #{room_name}: #{body}"
+
+    # create message
     ChatWork::Message.create room_id: destination_room_id, body: body
     render status: 200
   end
